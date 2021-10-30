4281eb30aa4a0d314a6ec2a9916a1e8c3d382293
1offmark <official1offmark@gmail.com>
2021-10-31 01:46:28 +0300

    added email configarations in settings.py

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +127,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
